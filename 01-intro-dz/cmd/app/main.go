34a6279f5fb0f82a251b61ea3ec11ea1d2434ce6
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-11-04 16:09:16 +0300

    feat: add local package

--- 01-intro-dz/cmd/app/main.go
+++ 01-intro-dz/cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/go-core-4/01-intro-dz/pkg/greetings"
 	"rsc.io/quote/v4"
 )
 
@@ -11,4 +12,5 @@ func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Opt())
+	fmt.Println(greetings.Hello("Vlad"))
 }
