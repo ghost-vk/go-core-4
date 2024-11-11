204ac2f83d8c41316ab83a8076171b7ba62922bf
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-11-11 10:49:37 +0300

    feat: first search iteration 🚀

---
+++ gosearch/README.md
@@ -0,0 +1,16 @@
+## CLI Args
+
+```
+$ go run cmd/main.go --help
+$ Usage of /var/folders/fv/3gqgb4yj0lscsb1gs9zqr_dm0000gn/T/go-build2271515170/b001/exe/main:
+  -s string
+        search word
+```
+
+## Example
+
+```
+$ go run cmd/main.go --s cli
+$ Command-line Interfaces (CLIs) - The Go Programming Language => https://go.dev/solutions/clis
+  Command-line Interfaces (CLIs) - The Go Programming Language => https://golang.org/solutions/clis
+```
--- gosearch/cmd/main.go
+++ gosearch/cmd/main.go
@@ -3,11 +3,40 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
+
+	"github.com/go-core-4/gosearch/pkg/crawler"
+	"github.com/go-core-4/gosearch/pkg/crawler/spider"
 )
 
 func main() {
 	in := parseInput()
-	fmt.Println(*in.s)
+
+	spider := spider.New()
+	pages1, err := scan(spider, "https://go.dev")
+	if err != nil {
+		fmt.Println("error go.dev scan page", err.Error())
+	}
+	pages2, err := scan(spider, "https://golang.org")
+	if err != nil {
+		fmt.Println("error scan golang.org page", err.Error())
+	}
+
+	for _, p := range append(pages1, pages2...) {
+		if strings.Contains(strings.ToLower(p.Title), in.s) {
+			fmt.Println(p.Title, "=>", p.URL)
+		}
+	}
+}
+
+func scan(searchService *spider.Service, url string) ([]crawler.Document, error) {
+	pages, err := searchService.Scan(url, 2)
+
+	if err != nil {
+		fmt.Printf("error %s scan page: %v\n", url, err.Error())
+	}
+
+	return pages, nil
 }
 
 func parseInput() Input {
@@ -15,10 +44,10 @@ func parseInput() Input {
 	flag.Parse()
 
 	return Input{
-		s: search,
+		s: strings.ToLower(*search),
 	}
 }
 
 type Input struct {
-	s *string
+	s string
 }
